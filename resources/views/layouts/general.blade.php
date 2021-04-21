f35e9df006fa2e3521df5fe4701ce44857afaf33
agathedkl <agathedekerle@yahoo.fr>
2021-04-21 13:37:20 +0200

    oauiriezr

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -8,48 +8,48 @@
 
 </head>
 <body>
-<nav>
-
-                            <a href='/'><img src="/images/uploads/1/oui.png"/></a>
-                            <a href='/about'>ABOUT</a>
-                            </nav>
+    <div class="container">
 @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                        <a href='/photos/create'>Create</a>
+    @if (Route::has('login'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+    
+    @endif
+    
+    @if (Route::has('register'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+    @endif
+    @else
+    <nav>
+        <a href='/'><img src="/assets/home.svg" alt="Home"/></a>
+        <a href='/photos/create'><img src="/assets/plus-circle.svg" alt="Create a Post"/></a>
+        <a href='/about'><img src="/assets/book.svg" alt="Herbarium"/></a>
+        <a href="/users/{{ Auth::user()->id }}"><img src="/assets/user.svg" alt="Profil"/></a>
+        
+    </nav>
+        <li>
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
 
-                            <li>
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                          
-                            <main>
-                                @yield('content')
-                            </main>
-                        @endguest
-
-
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </li>
+    
+    <main>
+        @yield('content')
+    </main>
+@endguest
+</div>
