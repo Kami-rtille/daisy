957e7a9d2c561a4414a40545a0f6d27933875991
Camille <camille.gcn@outlook.com>
2021-04-21 11:27:36 +0200

    Merge

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -1,9 +1,11 @@
 <html>
 <head>
 <meta charset='utf-8'>
-<title>Titre page</title>
+<title>Daisy</title>
 <link href='/css/style.css' type='text/css' rel="stylesheet" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
 <nav>
