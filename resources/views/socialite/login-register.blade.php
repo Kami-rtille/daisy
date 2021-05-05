f60f2f89d07c37b63f391a02b835c58db342574f
Camille <camille.gcn@outlook.com>
2021-05-05 22:19:30 +0200

    debut de welcome

--- resources/views/socialite/login-register.blade.php
+++ resources/views/socialite/login-register.blade.php
@@ -1,12 +1,5 @@
 
-<div class="socialite">
-	<h1>Se connecter / S'enregistrer avec un compte social</h1>
-	<p>
 		<!-- Lien de redirection vers Google -->
-		<a href="{{ route('socialite.redirect', 'google') }}" title="Connexion/Inscription avec Google" class="btn btn-link"  >Continuer avec Google</a>
-	
-		<!-- Lien de redirection vers Facebook -->
-		<a href="{{ route('socialite.redirect', 'facebook') }}" title="Connexion/Inscription avec Facebook" class="btn btn-link"  >Continuer avec Facebook</a>
+		<a class="google" href="{{ route('socialite.redirect', 'google') }}" title="Connexion/Inscription avec Google" class="btn btn-link"  >Continuer avec Google</a>		<!-- Lien de redirection vers Facebook -->
+		<a class="facebook" href="{{ route('socialite.redirect', 'facebook') }}" title="Connexion/Inscription avec Facebook" class="btn btn-link"  >Continuer avec Facebook</a>
 
-		</p>
-</div>
