3eb2347e63a33e88807a57c48a5a1b9006d099bd
Camille <camille.gcn@outlook.com>
2021-05-05 18:36:12 +0200

    fake filter

--- resources/views/firstcontroller/_photos.blade.php
+++ resources/views/firstcontroller/_photos.blade.php
@@ -5,10 +5,11 @@
             <a class="name" href="users/{{$p->user->id}}">{{$p->user->name}}</a>
             <img src="{{$p->url}}" alt="{{$p->title}}" />
             </div>
-            <span>{{$p->title}}</span>
-            <span>{{$p->spec1}}</span>
-            <span>{{$p->spec2}}</span>
-
+            <div class="specs">
+            <span class="yellow" >{{$p->title}}</span>
+            <span class="lightgreen">{{$p->spec1}}</span>
+            <span class="green">{{$p->spec2}}</span>
+</div>
             <!-- <p>{{$p->user->name}}</p> -->
             
         </div>
